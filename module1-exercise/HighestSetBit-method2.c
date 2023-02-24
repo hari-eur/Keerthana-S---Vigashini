2a081d4f1e25149487f911cfde8ea78c8d16b26c
Keerthana S <54321645+keerthana-5170@users.noreply.github.com>
2023-02-24 18:14:38 +0530

    Create HighestSetBit-method2.c

---
+++ module1-exercise/HighestSetBit-method2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    long int count=0,m;
+    printf("Enter a number:");
+    scanf("%ld",&m);
+    if(m==0)
+    {
+        return 0;
+    }
+    m=num;    
+    m=m/2;
+    while(m!=0)
+    {
+        m=m/2;
+        count++;
+        
+    }
+    printf("msb =%d",1<<count);
+   
+}
+/*sample i/o
+  Enter a number: 10
+  msb=8
+  */
