11b3c623a3ad244df633a630f025f4773afe0b84
Keerthana S <54321645+keerthana-5170@users.noreply.github.com>
2023-03-08 12:26:36 +0530

    Create Pyramids.c

---
+++ module2-exercise/Pyramids.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<string.h>
+void pyramid(int n);
+void invertedPyramid(int n);
+void halfPyramid(int n);
+int main()
+{
+    int rows,i=0;
+    printf("enter number of rows\n");
+    scanf("%d",&rows);
+    pyramid(rows);
+    halfPyramid(rows);
+    invertedPyramid(rows);
+    return 0;
+}
+void pyramid(int n)
+{
+    printf("\n Full Pyramid\n");
+
+    int i=0;
+     while(i<=n)
+    {
+        for(int k=0;k<n-i;k++)
+        {
+            printf(" ");
+        }
+        for(int j=0;j<i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+        i++;
+    }
+}
+
+void halfPyramid(int n)
+{
+       int  i=0;
+        printf("\n Half Pyramid\n");
+        while(i<=n)
+    {
+       
+        for(int j=0;j<i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+        i++;
+    }
+      printf("\n");
+    
+}
+void invertedPyramid(int n)
+{
+     int i=0;
+     printf("\n Inverted Pyramid\n");
+        while(i<=n)
+    {
+        for(int k=n-i;k<n;k++)
+        {
+            printf(" ");
+        }
+        for(int j=0;j<n-i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+        i++;
+    }
+          
+
+}
