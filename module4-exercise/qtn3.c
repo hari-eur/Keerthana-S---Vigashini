ee53c580009ce6e5965b4625851629f359b9d7ed
Keerthana S <54321645+keerthana-5170@users.noreply.github.com>
2023-03-29 11:08:20 +0530

    Create qtn3.c

---
+++ module4-exercise/qtn3.c
@@ -0,0 +1,19 @@
+//3. Create a fixed array and use pointers to transverse through elements and print the output.
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    int *ptr=&arr[0];
+    for(int i=0;i<5;i++)
+    {
+        printf("%d ",*ptr);
+        ptr++;
+    }
+    return 0;
+}
+
+/* Sample i/o
+1 2 3 4 5 
+*/
